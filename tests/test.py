fbf1b4686be43707eb9f42c733321a253bb21d07
tomasuIo <592634894@qq.com>
2017-12-12 20:38:35 +0800

    一些添加

---
+++ tests/test.py
@@ -0,0 +1,38 @@
+
+str = "afdsfff,gfdgfdgf."
+
+list =["asdfasdfads"]
+
+list1  =str.split(',')
+
+#print(list1)
+
+
+dic = {"n":"I","k":"person name"}
+
+#for i, value in dic:
+ #   print(i)
+
+
+
+
+def test():
+    return "asdfasdfasdfasdf"
+
+
+
+string = "asdfasdf.txt"
+
+if string.endswith('.txt'):
+    print(string)
+
+
+
+def test3(*params):
+    for item in params:
+        print(item)
+
+test3("adsfa","fff","fffeaff")
+
+
+cx = 330
